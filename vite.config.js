2dfac02023c6774a62aa1ab2bf345a30e3db837a
Barnabas Tan <bttf243@gmail.com>
2025-07-31 18:17:22 +0800

    formatting

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,33 @@
-import laravel from 'laravel-vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig, loadEnv } from 'vite'
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, process.cwd(), "");
     return {
         build: {
             rollupOptions: {
                 output: {
                     manualChunks: (id) => {
-                        if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
+                        if (id.includes("node_modules"))
+                            return id
+                                .toString()
+                                .split("node_modules/")[1]
+                                .split("/")[0]
+                                .toString();
+                    },
+                },
+            },
         },
         plugins: [
             tailwindcss(),
             laravel({
                 refresh: true,
-                input: [
-                    'resources/css/site.css',
-                    'resources/js/site.js',
-                ]
-            })
+                input: ["resources/css/site.css", "resources/js/site.js"],
+            }),
         ],
         server: {
-            open: env.APP_URL
-        }
-    }
+            open: env.APP_URL,
+        },
+    };
 });
