b860371f1b647e88cc0b264580fb1914e8f7c066
Barnabas Tan <bttf243@gmail.com>
2025-09-03 02:55:43 +0800

    fix: set consistent tracesSampleRate for Sentry configuration

--- resources/js/sentryConf.js
+++ resources/js/sentryConf.js
@@ -3,6 +3,6 @@ import * as Sentry from "@sentry/browser";
 Sentry.init({
     dsn: "https://b4bdfb3e20eb9dd5a6c00f3b32d8c9a9@o4507669075460096.ingest.de.sentry.io/4509951344574544",
     integrations: [Sentry.browserTracingIntegration()],
-    tracesSampleRate: import.meta.env.PROD ? 0.1 : 1.0, // lower in prod
+    tracesSampleRate: 0.1,
     tracePropagationTargets: ["lscc.org.sg", /^https:\/\/lscc\.org\.sg\/api/],
 });
