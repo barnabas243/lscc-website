2c5c9c1bfe41d9164fc938278c57a7e6c97119fa
Barnabas Tan <bttf243@gmail.com>
2025-09-03 02:25:05 +0800

    feat: add Sentry configuration for error tracking and performance monitoring

---
+++ resources/js/sentryConf.js
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+    dsn: "https://b4bdfb3e20eb9dd5a6c00f3b32d8c9a9@o4507669075460096.ingest.de.sentry.io/4509951344574544",
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: import.meta.env.PROD ? 0.1 : 1.0, // lower in prod
+    tracePropagationTargets: ["lscc.org.sg", /^https:\/\/lscc\.org\.sg\/api/],
+    environment: import.meta.env.PROD ? "production" : "development",
+    release: "lscc-website@" + (import.meta.env.VITE_APP_VERSION ?? "dev"),
+});
